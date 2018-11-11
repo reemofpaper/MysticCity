f1ef42c73baf93ba04b5d6d0d9db007a4edcb809
jhorto5 <43076969+jhorto5@users.noreply.github.com>
2018-11-11 01:05:35 -0600

    jhorto5 HW3 code

--- UseMove.java
+++ UseMove.java
@@ -16,6 +16,16 @@ public class UseMove extends Move {
   
   @Override 
   public void execute(){
+    Artifact a = c.has_Artifact(s);
+      
+    if (a != null){
+      p.useKey(a);
+      System.out.println(c.name() + " used artifact " + this.s);
+    }
+    else {
+      System.out.println(c.name() + " does not have artifact " + this.s + ". Sorry");
+    }
     
   }
+  
 }

6c401400d79c657269c010ab1ada71636fac0806
jhorto5 <43076969+jhorto5@users.noreply.github.com>
2018-11-11 00:21:56 -0600

    Create InventoryMove.java

---
+++ InventoryMove.java
@@ -0,0 +1,17 @@
+/*
+name: Joshua Horton
+netID: jhorto5
+*/
+
+public class InventoryMove extends Move {
+  private Character c;
+  
+  public InventoryMove(Character c){
+    this.c = c;
+  }
+  
+  @Override
+  public void execute(){
+    
+  }
+}

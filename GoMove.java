68a792da8ef75d5eef5588d91f2d2087cadc3689
jhorto5 <43076969+jhorto5@users.noreply.github.com>
2018-11-10 23:01:37 -0600

    jhorto5 HW3 code

---
+++ GoMove.java
@@ -0,0 +1,32 @@
+/*
+name: Joshua Horton
+netID; jhorto5
+*/
+
+public class GoMove extends Move {
+   private Character c;
+   private Place p;
+   private String direction;
+   
+   public GoMove(Place p, String direction, Character c){
+    this.p = p;
+    this.direction = direction;
+    this.c = c;
+   }
+   
+   @Override
+   public void execute(){
+    Place newplace = p.followDirection(direction);
+    
+  	if (newplace.equals(this.p)) {
+      System.out.println(c.name() + " is in " + newplace.name()+ "\n");
+    }
+    else {
+   	  p.removeCharacter(c);
+   	  System.out.println(c.name() + " moved from " + p.name() + " to " + newPlace.name() + "\n");
+    }
+    
+    newPlace.addCharacter(c);
+    c.setCurrentPlace(newPlace);
+   }
+}

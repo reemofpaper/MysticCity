2ca107301f5321563a434654dd8438b49eb1bcae
jhorto5 <43076969+jhorto5@users.noreply.github.com>
2018-11-11 08:20:24 -0600

    jhorto5 HW3 code

---
+++ QuitMove.java
@@ -0,0 +1,17 @@
+/*
+name: Joshua Horton
+netID: jhorto5
+*/
+
+public class QuitMove extends Move {
+  private Character c;
+  
+  public QuitMove(Character c){
+    this.c = c;
+  }
+  
+  @Override
+  public void execute(){
+    //implement this
+  }
+}

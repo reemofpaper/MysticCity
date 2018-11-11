30b627df593454e4271045ea8896985a3d086aa7
jhorto5 <43076969+jhorto5@users.noreply.github.com>
2018-11-10 22:09:58 -0600

    jhorto5 HW3 code

---
+++ getMove.java
@@ -0,0 +1,27 @@
+/*
+name: Joshua Horton
+netID: jhorto5
+/*
+
+public class getMove extends Move{
+   private Character c;
+   private Place p;
+   private String s; //artifact name
+   
+   public getMove(Character c, Place p, String s){
+     this.p = p;
+     this.c = c;
+     this.s = s;
+     return;
+   }
+   
+   @Override
+   public void execute(){
+     Artifact s = p.removeArtifactByName(s);
+     if(a!=null){
+			  c.addArtifact(s); 
+			  System.out.println("\n(" + c.name() + " grabbed " + s.name() + ")");
+		 }
+     return;
+   }
+}

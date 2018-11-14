efda0aa5b324a1022bae352c4360f8243b7426e3
jhorto5 <jhorto5@uic.edu>
2018-11-13 23:06:47 -0600

    added UseKey

---
+++ UseKey.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class UseKey extends Artifact {
+  private Character c;
+  private Place p;
+
+  //take in Artifact constructor
+  public UseKey(Scanner infile, Character x, Place y){
+        super(infile);
+        c = x;
+        p = y;
+  }
+
+  @Override
+  public void use(Character c, Place p){
+
+        //getting each key by keyPattern
+        while(keyPattern() > 1000){
+          //check if it's a NPC
+          if(c.characterID() < 10){
+                System.out.println(c.name() + " is not allowed to use that!");
+          }
+          else{
+                p.useKey(this);
+          }
+        }
+  }
+
+}

f3b5bc77d399f6ca64f62cf5f53c97663d25f27a
Reem Hussein <reemyhussein@gmail.com>
2018-12-05 14:58:40 -0600

    removed files

--- DecisionMaker.java
+++
@@ -1,8 +0,0 @@
-// Reem Hussein, rhussein
-// Maleeha Ahmed, mahmed
-// Joshua Horton, jhorton
-// CS 342 Project 4
-
-public interface DecisionMaker{
-  public Move getMove(Character c, Place p);
-}
